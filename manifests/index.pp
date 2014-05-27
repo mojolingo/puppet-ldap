1ed9a1a35c1f7c06f6d98f771199642de4b01a88
Ben Langfeld <ben@langfeld.me>
2014-05-26 21:14:24 -0300

    Fix definition

--- manifests/index.pp
+++ manifests/index.pp
@@ -1,13 +1,11 @@
 define ldap::index() {
   include ldap
 
-  define index() {
-    ldapdn { "${name} index":
-      dn          => "olcDatabase={1}bdb,cn=config",
-      attributes  => [
-        "olcDbIndex: ${name}"
-      ],
-      ensure      => present,
-    }
+  ldapdn { "${name} index":
+    dn          => "olcDatabase={1}bdb,cn=config",
+    attributes  => [
+      "olcDbIndex: ${name}"
+    ],
+    ensure      => present,
   }
 }
