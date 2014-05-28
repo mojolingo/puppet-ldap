350bfafb4d692c89876547fb8d8b08eec4e3a89e
Ben Langfeld <ben@langfeld.me>
2014-05-28 16:31:56 -0300

    We need to be sure the directory exists

--- manifests/server/convertschema.pp
+++ manifests/server/convertschema.pp
@@ -1,5 +1,11 @@
 class ldap::server::convertschema() {
 
+  file { $ldap::params::prefix:
+    owner   => $ldap::params::server_owner,
+    group   => $ldap::params::server_group,
+    ensure  => directory,
+  }
+
   # Upload the conversion script for the schemas
   file { "${ldap::params::prefix}/convertschema.sh":
     owner   => 'root',
